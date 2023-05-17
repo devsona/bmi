52560a968600cf06e77e188b98c0f139467158f3
devsona <122227306+devsona@users.noreply.github.com>
2023-05-16 19:39:22 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let height, weight;
+let height, weight
 
 function changeUnit() {
   let unit = document.getElementById('radio-container');
@@ -6,9 +6,9 @@ function changeUnit() {
     if (e.target.id == 'metric') {
       document.getElementById('wxh-container').innerHTML = `        
         <p>height</p>
-        <input type="number" class="wxh" placeholder="CM" id='cm'>
+        <input type="number" class="wxh" placeholder="CM" id='cm' min="1" max="300"> 
         <p>weight</p>
-        <input type="number" class="wxh" placeholder="KG" id="weight">`;
+        <input type="number" class="wxh" placeholder="KG" id="weight"  min="1" max="999">`;
     } else {
       document.getElementById('wxh-container').innerHTML = `             
         <p>height</p>
@@ -45,18 +45,56 @@ function getBmi() {
     bmi = fixedDecimal(bmi);
     return bmi;
   } else {
-    // Calculate BMI for imperial units
-    // ...
+    let feet = document.getElementById('feet').value;
+    let inch = document.getElementById('inch').value;
+    let st = document.getElementById('st').value;
+    let pound = document.getElementById('pound').value;
+    
+    let heightInInches = (parseInt(feet) * 12) + parseInt(inch);
+    let weightInPounds = (parseInt(st) * 14) + parseInt(pound);
+    
+    let bmi = weightInPounds / Math.pow(heightInInches, 2) * 703;
+    bmi = fixedDecimal(bmi);
+    return bmi;
   }
 }
 
-function addBtn() {
-  let bmi = getBmi();
-  if (weight && height) {
-    document.getElementById('submit-container').innerHTML = `<button>Submit</button>`;
-  }
+function ifHealthy(){
+    let bmi = getBmi()
+    let healthyWord;
+    if(bmi < 18.5){
+        healthyWord = 'Underweight'
+    } else if (bmi <= 24.5){
+        healthyWord = 'Healthy'
+    } else if (bmi <= 30) {
+        healthyWord = 'Overweight'
+    } else if (bmi > 30){
+        healthyWord = 'Obese'
+    }else {
+        healthyWord = 'error please try again!'
+    }
+    return healthyWord   
 }
+
+
+
 
 document.addEventListener('input', function() {
-    addBtn();
+    let numberHtml = document.getElementById('bmi-answer');
+    let bmi = getBmi();
+    
+    if (isNaN(bmi) || !isFinite(bmi)) {
+      numberHtml.innerHTML = '0.00';
+      document.getElementById('bmi-answer-p-ending').innerHTML =`Add your weight and height details to recive more information about your bmi. Or scroll down to learn more!`
+    } else {
+      numberHtml.innerHTML = bmi;
+      document.getElementById('bmi-answer-p-ending').innerHTML =`Your BMI suggests you’re a <span>${ifHealthy()}</span> weight. `
+    }
+    if (bmi > 100 || bmi < 0){
+        numberHtml.innerHTML = 'Impomsible BMI'
+        numberHtml.style.fontSize = '2rem'
+    }
+
 });
+
+
